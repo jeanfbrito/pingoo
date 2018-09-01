a841460ef7291762364cb2a21b6de4b8f412555d
Jean Brito <jeanfbrito@gmail.com>
2018-08-31 21:46:20 -0300

    migrate watchers

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_09_01_004424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "watchers", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.string "current_status"
+    t.string "previous_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
