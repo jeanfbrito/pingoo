d509dd32a2cab6545fca759e1f949cd99e573fd3
Jean Brito <jeanfbrito@gmail.com>
2018-09-01 07:15:41 -0300

    add watch_it method

--- app/models/watcher.rb
+++ app/models/watcher.rb
@@ -1,2 +1,12 @@
 class Watcher < ApplicationRecord
+  def watch_it
+    require "http"
+    self.previous_status = self.current_status
+    previous_response = current_response
+    response = HTTP.get(self.url)
+    self.current_status = response.code.to_s
+    self.current_response = response.to_s
+    self.last_watched = Time.zone.now
+    self.save!
+  end
 end
