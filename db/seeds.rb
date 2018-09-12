3d7b3ce2675e38d05cd9831a35197a9a73ae1a18
Patrick Müller <patrick.e.muller@gmail.com>
2018-09-12 12:19:00 -0300

    Add release phase file for Heroku & Docker startup files rails sidekiq

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do |index|
+  Watcher.create(
+    name: "Worker #{index}",
+    url: 'https://google.com',
+    timer: rand(1..5)
+  )
+end
